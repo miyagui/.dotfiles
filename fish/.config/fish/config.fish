1b73b632aa34d5c3ff9aaa9596e7ac644de10d7b
Miya <h2llprint@gmail.com>
2022-07-24 22:37:30 -0500

    updating scripts

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,3 +20,9 @@ if status is-interactive
 end
 
 set fish_greeting ""
+
+alias vimwiki="nvim -c VimwikiIndex"
+alias grubo="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias lolo="sh -c 'sysctl -w abi.vsyscall32=0'"
+alias pythonserve="python -m http.server 8000"
+alias shadowplay="gpu-screen-recorder -w DP-4 -c mp4 -f 60 -a "EVO4.monitor" -o /mnt/speed/zHome/Videos/_loonix/apexio-$(date +"%Y-%m-%d-%H%M").mp4"
